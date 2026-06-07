d3a2f9207a275f6ecad1f7d2d5c9c6914946fb4b
Bharat Kathi <bharat1031@gmail.com>
2026-06-06 17:49:10 -0700

    release: foreman v2.0.0

--- config/config.go
+++ config/config.go
@@ -21,7 +21,7 @@ func (s ServiceInfo) PathPrefix() string {
 
 var Service = ServiceInfo{
 	Name:    "Foreman",
-	Version: "1.0.0",
+	Version: "2.0.0",
 }
 
 // All endpoints are public for now — no auth layer. Re-add a
